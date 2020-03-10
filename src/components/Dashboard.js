ee7132ce030a28e706502e2bd77acd7d2d5ec4d4
marcusjoesting <mjoesting@ugpinc.com>
2020-03-10 18:04:34 -0400

    UI EVENT SEARCH 0.0.1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,7 +21,7 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import LinkList from "./LinkList";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import Menu from './Menu'
-import Example from "./Example";
+import Example from "./Tournaments";
 
 function Copyright() {
     return (
--- src/components/Tournaments.js
+++ src/components/Tournaments.js
@@ -1,60 +1,115 @@
 import React from 'react'
+import uuidv4 from 'uuid/v4'
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import {makeStyles} from "@material-ui/core/styles";
+import { useQuery } from '@apollo/react-hooks'
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
-import {fakeTournies} from '../MOCK_DATA'
-import _ from 'lodash'
-
-
-
-export default function Tournaments(props) {
-    const [data, setData] = React.useState(fakeTournies)
-
-    const sortEntrants = () => {
-        let arr = [...data]
-        arr = _.sortBy(arr, x => { return x.name})
-        setData(arr)
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import {TommiesFirstqueeere} from "./Queries";
+import SearchIcon from '@material-ui/icons/Search'
+import Loader from 'react-loader-spinner'
+const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: theme.spacing(2),
+        margin: theme.spacing(2),
+        width: '100%'
     }
+}))
+export function Tournaments(props) {
+    const classes = useStyles()
+    const [standings, setStandings] = React.useState([])
+    const [filtered, setFiltered] = React.useState([])
+    const [search, setSearch] = React.useState('')
+
+    const { loading, error, data } = useQuery(TommiesFirstqueeere, {
+        variables: {
+            eventId: props.eventId ? props.eventId : 78790,
+            page: 1,
+            perPage: 100
+        }
+    })
+
+
+    React.useEffect(() => {
+        if (data) {
+            console.log(data)
+        }
+        if (data && data.event &&   data.event.standings && data.event.standings.nodes) {
+            setStandings(data.event.standings.nodes)
+            console.log(data.event.standings.nodes)
+            setFiltered(data.event.standings.nodes)
+        }
+    },[data])
+
+
+    //
+    React.useEffect(() => {
+        let arr = [...standings]
+        arr = arr.filter(x => {
+            return (x.entrant.name.toLowerCase().indexOf(search.toLowerCase()) !== -1)
+        })
+        setFiltered(arr)
+
+    }, [search])
+
+
 
     return(
-        <Grid container justify={'center'} item xs={12}>
-            <Grid item xs={3}>
-                <Typography variant={'h5'}>Tournament Name</Typography>
-            </Grid>
-            <Grid item xs={3}>
-                <Typography variant={'h5'}>Winner</Typography>
-            </Grid>
-            <Grid item xs={3}>
-                <Typography variant={'h5'} onClick={() => sortEntrants()}>Entrants</Typography>
-            </Grid>
-            <Grid item xs={3}>
-                <Typography variant={'h5'}>Date</Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Divider/>
-            </Grid>
-            {data.map(tourney => {
-                return(
-                    <>
-                        <Grid item xs={3}>
-                            {tourney.name}
-                        </Grid>
-                        <Grid item xs={3}>
-                            {tourney.winner}
-                        </Grid>
-                        <Grid item xs={3}>
-                            {tourney.entrants}
-                        </Grid>
-                        <Grid item xs={3}>
-                            {tourney.date}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Divider/>
-                        </Grid>
+        <Paper className={classes.paper}>
+            <Grid container spacing={2} justify={'center'}>
 
-                    </>
-                )
-            })}
-        </Grid>
+                {loading && <Loader type={'Circles'} height={100} width={100} color={'black'}/>}
+                {error && <h1>Error! SOMETHING IS WRONG</h1>}
+                <Grid item xs={12}>
+                    <TextField label='Search Player' variant={'outlined'} fullWidth value={search} onChange={(e) => setSearch(e.target.value)}/>
+                </Grid>
+                {filtered.map(standing => {
+                    return (
+                        <React.Fragment key={uuidv4()}>
+                            <Grid item xs={6}>
+                                <Typography align={'center'}>{standing.placement}</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography align={'left'}>
+                                    {standing.entrant.name}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider/>
+                            </Grid>
+                        </React.Fragment>
+                    )
+                })}
+            </Grid>
+        </Paper>
+    )
+}
+export default function TourneyWrapper(props) {
+    const classes = useStyles()
+    const [eventId, setEventId] = React.useState(78790)
+    const [submit, setSubmit] = React.useState(false)
+    return (
+        <Paper className={classes.paper}>
+            <Grid container item xs={12} justify={'center'} spacing={2} alignItems={'center'}>
+
+                <Grid item xs={12} md={6}>
+                    <TextField fullWidth value={eventId} onChange={(e) => setEventId(e.target.value)} variant={'outlined'} label={'Event ID'}/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Button onClick={() => setSubmit(!submit)} fullWidth size={'large'}><SearchIcon/></Button>
+                </Grid>
+                <Grid item xs={12}>
+                    {React.useMemo( () => {
+                        return (
+                            <Tournaments eventId={eventId}/>
+                        )
+                    }, [submit])}
+                </Grid>
+            </Grid>
+        </Paper>
+
     )
 }
