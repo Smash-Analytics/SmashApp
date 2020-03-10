8db8616b23f96c93c51bfdf5bf969b6704f4bec2
marcusjoesting <mjoesting@ugpinc.com>
2020-03-10 16:47:13 -0400

    Material UI

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import uuidv4 from 'uuid/v4'
 import gql from 'graphql-tag'
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import {makeStyles} from "@material-ui/core/styles";
 import { useQuery } from '@apollo/react-hooks'
-
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import TextField from "@material-ui/core/TextField";
 const FEED_QUERY = gql`
 query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
     event(id: $eventId) {
@@ -24,16 +29,25 @@ query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
   }
   `
 
+const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: theme.spacing(2),
+        margin: theme.spacing(2),
+    }
+}))
 export default function Example(props) {
+    const classes = useStyles()
+    const [standings, setStandings] = React.useState([])
+    const [eventId, setEventId] = React.useState(0)
     const { loading, error, data } = useQuery(FEED_QUERY, {
         variables: {
-            eventId: 78790,
+            eventId: eventId,
             page: 1,
-            perPage: 20
+            perPage: 100
         }
     })
+    console.log(error)
 
-    const [standings, setStandings] = React.useState([])
 
     React.useEffect(() => {
         if (data && data.event && data.event.standings.nodes) {
@@ -44,15 +58,29 @@ export default function Example(props) {
 
 
     return(
-        <div>
-            {standings.map(standing => {
-                console.log(standing)
-                return (
-                    <div key={uuidv4()}>
-                        {standing.placement} - {standing.entrant.name}<br/>
-                    </div>
-                )
-            })}
-        </div>
+        <Paper className={classes.paper}>
+            <Grid container spacing={2}>
+                {loading && <h1>LOADING...</h1>}
+                {error && <h1>Error! SOMETHING IS WRONG</h1>}
+                {standings.map(standing => {
+                    return (
+                        <React.Fragment key={uuidv4()}>
+                            <TextField fullWidth value={eventId} onChange={(e) => setEventId(e.target.value)}/>
+                            <Grid item xs={6}>
+                                <Typography align={'center'}>{standing.placement}</Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography align={'left'}>
+                                    {standing.entrant.name}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Divider/>
+                            </Grid>
+                        </React.Fragment>
+                    )
+                })}
+            </Grid>
+        </Paper>
     )
 }
