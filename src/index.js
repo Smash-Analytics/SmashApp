2084139c215caf67ba65f7daf565f88749832a11
Thomas Williams <tawilliams1995@gmail.com>
2020-03-09 22:43:48 -0400

    authorization/access
    i first tried to add the authorization steps to be able to query not in the development cycle, but it doesn't work. No clue what to do, but i am pushing it anyways because i feel like i made some progress.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//apollo
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//link apollo client with API
+const httpLink = createHttpLink({
+  uri: 'https://api.smash.gg/gql/alpha'
+})
+
+//set header authorization
+const authLink = setContext((_, { headers }) => {
+    // token is auth token from smashgg
+    const token = "0d07dd1dbafe6a5350dde1f97f20ddca";
+    // return the headers to the context so httpLink can read them (no clue what I am doing)
+    return {
+      headers: {
+        ...headers,
+        authorization: Bearer [token],
+      }
+    }
+  });
+  
+  //instantiate apollo client
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
+  //renders root components
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  )
 serviceWorker.unregister();
