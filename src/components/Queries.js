37b19ba42abfd17c723433ca0d31f94c95a0e281
Thomas Williams <tawilliams1995@gmail.com>
2020-03-10 18:11:42 -0400

    added a query

--- src/components/Queries.js
+++ src/components/Queries.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 
-export const TommiesFirstqueeere = gql`
+export const EventStandings = gql`
 query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
     event(id: $eventId) {
       id
@@ -21,3 +21,32 @@ query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
     }
   }
   `
+
+  export const TournamentsByVideogame = gql`
+  query TournamentsByVideogame($perPage: Int!, $videogameId: ID!) {
+    tournaments(query: {
+      perPage: $perPage
+      page: 1
+      sortBy: "startAt desc"
+      filter: {
+        past: false
+        videogameIds: [
+          $videogameId
+        ]
+      }
+    }) {
+      nodes {
+        id
+        name
+        slug
+        startAt
+        events {
+          name
+          id
+          videogame {
+            id
+          }
+        }
+      }
+    }
+  }`  
